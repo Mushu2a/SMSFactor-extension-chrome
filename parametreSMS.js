86c4f77930a3e008c4033c8f348b5da57561f356
Mushu2a <lastennet.l@gmail.com>
2015-05-30 17:27:53 +0200

    Ajout du javascript permettant d'envoyé le XML à l'API SMSFactor

---
+++ parametreSMS.js
@@ -0,0 +1,41 @@
+/*
+* Configure le XML pour envoyé le SMS au destinataire
+* Configure XML for send SMS at recipient
+*/
+
+// Envois XML avec les informations
+// Send XML with informations
+function sendXML() {
+	var login = document.getElementById('login').value;
+	var password = document.getElementById('password').value;
+	var sender = document.getElementById('sender').value;
+	var destinataire = document.getElementById('destinataire').value;
+	var message = document.getElementById('message').value;
+
+	document.sendSMS.XML.value = "<sms><authentification><username>" + login + "</username><password>" + password + "</password></authentification><message><sender>" + sender +"</sender><text>" + message + "</text></message><recipients><gsm gsmsmsid=\"\">" + destinataire +"</gsm></recipients></sms>" ;
+	document.getElementById('sendSMS').submit();
+}
+
+// Affiche les informations enregistrer
+// Show informations register
+function main() {
+	var login = document.getElementById('login');
+	var password = document.getElementById('password');
+	var sender = document.getElementById('sender');
+
+	if (typeof Storage !== "undefined") {
+		login.value = localStorage['SMSFactorlogin'];
+		password.value = localStorage['SMSFactorpassword'];
+		sender.value = localStorage['SMSFactorsender'];
+	}
+}
+
+window.addEventListener("load", main);
+
+document.addEventListener('DOMContentLoaded', function () {
+	var send = document.getElementById('envois');
+
+	if(send){
+		send.addEventListener('click', sendXML, false);
+	}
+});
